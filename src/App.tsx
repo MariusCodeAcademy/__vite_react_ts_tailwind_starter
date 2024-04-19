a50d54d3cf4380d3093f29c52cfb00d6484b26ed
MariusMokymai <testams752@gmail.com>
2024-04-19 17:58:42 +0300

    sort classes navbar + img

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 //
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className='container'>
-      <h1 className='text-5xl font-medium underline'>App</h1>
+    <div className=''>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="container relative p-6">
+      <div className="flex items-center justify-between">
+        <div className="pt-2"></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
