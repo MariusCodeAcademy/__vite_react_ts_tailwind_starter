60e88d70cae34a913bd33a0cf32e55e3a9b46dc1
MariusMokymai <testams752@gmail.com>
2024-04-19 18:11:28 +0300

    nav done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,42 @@ const Navbar = () => {
   return (
     <nav className="container relative p-6">
       <div className="flex items-center justify-between">
-        <div className="pt-2"></div>
+        <div className="pt-2">
+          <img src="img/logo.svg" alt="logo" />
+        </div>
+        <ul className="hidden md:flex">
+          <li>
+            <a className="px-3 py-2 hover:text-darkGrayishBlue" href="/">
+              Pricing
+            </a>
+          </li>
+          <li>
+            <a className="px-3 py-2 hover:text-darkGrayishBlue" href="/">
+              Product
+            </a>
+          </li>
+          <li>
+            <a className="px-3 py-2 hover:text-darkGrayishBlue" href="/">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a className="px-3 py-2 hover:text-darkGrayishBlue" href="/">
+              Careers
+            </a>
+          </li>
+          <li>
+            <a className="px-3 py-2 hover:text-darkGrayishBlue" href="/">
+              Community
+            </a>
+          </li>
+        </ul>
+        <a
+          href="/"
+          className="rounded-full bg-brightRed px-6 py-3 pt-2 text-white hover:bg-brightRedLight "
+        >
+          Get started
+        </a>
       </div>
     </nav>
   );
